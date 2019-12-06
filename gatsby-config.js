f0c8af4332e6863a2225b14f50930fbf8e8bc6fd
Benjamin Rudolf von Rohr <benjamin.rudolfvonrohr@gmail.com>
2019-12-06 13:57:05 +0100

    add environment configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
+let activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+console.log(`Using environment config: '${activeEnv}'`)
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+});
 module.exports = {
   siteMetadata: {
     title: `My Awesome Blog`,
@@ -31,8 +36,8 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: `bldtk2ft045j`,
-        accessToken: `_9QwMj9sqxH8-U7SdJQci_BmGQLDjrO1bg4BvfzEDPQ`,
-        host: `preview.contentful.com`
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+        host: process.env.GATSBY_CONTENTFUL_HOST
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
